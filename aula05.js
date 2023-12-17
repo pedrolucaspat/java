3f8e622faefae589f8111c6dd5bfd34973acd27b
Pedro lucas <153926798+pedrolucaspat@users.noreply.github.com>
2023-12-17 20:36:26 -0300

    update aula05.js

---
+++ aula05.js
@@ -0,0 +1,20 @@
+function numeros(numero1, numero2){
+  let menor = 0;
+  let maior = 0;
+  let soma = 0;
+  if(numero1 > numero2){
+    menor = numero2;
+    maior = numero1;
+    
+  }else{
+    menor = numero1;
+    maior = numero2;
+  }
+  for(menor; menor<maior; menor++){
+    if(menor % 13 !== 0){
+      soma += menor;
+    }
+  }
+  return soma;
+}
+console.log(numeros(200, 100))
