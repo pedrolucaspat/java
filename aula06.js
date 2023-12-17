d03656912ff927d2f81254204ca2f0e131ea83de
Pedro lucas <153926798+pedrolucaspat@users.noreply.github.com>
2023-12-17 20:52:15 -0300

    update aula06.js

---
+++ aula06.js
@@ -0,0 +1,10 @@
+//manipulando strings//
+const string = "ola mundo"
+const string2 = "eu sou o pedro"
+
+console.log("concat: " + string.concat(string2))
+console.log("concat +: " + string +" "+ string2)
+console.log("ES: " + `${string} ${string2}`)
+console.log("LOW: " + string2.toLowerCase())
+console.log("UP: " + string2.toUpperCase())
+
